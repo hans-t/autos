073e3cf9e6813db146508b12fea4bb72562c84f2
Zalora-hans <hans.t@zalora.com>
2016-08-25 23:00:03 +0800

    Adds unit tests for Drive. New methods and improvement for Drive

--- autos/googleapi/drive.py
+++ autos/googleapi/drive.py
@@ -1,9 +1,13 @@
 """
-Documentation: https://developers.google.com/resources/api-libraries/documentation/drive/v3/python/latest/index.html
+See:
+https://developers.google.com/resources/api-libraries/documentation/drive/v3/python/latest/index.html
+https://developers.google.com/drive/v3/reference/
+https://developers.google.com/drive/v3/web/examples/
 """
 
 
 import os
+import time
 import logging
 
 from apiclient.http import MediaFileUpload
@@ -19,27 +23,42 @@ from .errors import HttpError
 logger = logging.getLogger(__name__)
 
 
+MAX_RETRIES = 15
+
+
 def incremental_download(fp, request):
-    try:
-        downloader = MediaIoBaseDownload(fp, request)
-        done = False
-        while not done:
-            _, done = downloader.next_chunk()
-    except HttpError as e:
-        logger.exception('DOWNLOAD_ERROR')
-        raise DownloadError from e
+    downloader = MediaIoBaseDownload(fp, request)
+    next_chunk = downloader.next_chunk
+    done = False
+    num_retries = 0
+    while not done:
+        try:
+            _, done = next_chunk()
+        except HttpError as e:
+            if e.resp.status in (500, 502, 503, 504) and num_retries < MAX_RETRIES - 1:
+                time.sleep(2 ** num_retries)
+                num_retries += 1
+                continue
+            else:
+                logger.exception('DOWNLOAD_ERROR')
+                raise DownloadError from e
+        else:
+            if num_retries:
+                num_retries = 0
 
 
 class Drive(Service):
     def __init__(
         self,
         scope='https://www.googleapis.com/auth/drive',
-        api_name='drive',
-        api_version='v3'
     ):
-        super().__init__(scope=scope, api_name=api_name, api_version=api_version)
+        super().__init__(
+            scope=scope,
+            api_name='drive',
+            api_version='v3',
+        )
 
-    def upload(self, src, mime_type=None, name=None, parents=()):
+    def upload(self, src, name=None, parents=(), mime_type=None):
         """Uploads a file to Google Drive and convert it to a Google document.
 
         Google Drive documentations:
@@ -82,8 +101,8 @@ class Drive(Service):
             raise UploadError from e
         return file.get('id')
 
-    def import_csv_as_gsheets(self, src, name=None, parents=()):
-        """Uploads a CSV file to Google drive and convert it to Google Sheet.
+    def import_csv_as_gsheet(self, src, name=None, parents=()):
+        """Uploads a CSV file to Google drive as Google Sheet.
 
         :type src: string
         :param src: Path to the csv file to be uploaded.
@@ -105,6 +124,29 @@ class Drive(Service):
             parents=parents,
         )
 
+    def import_docx_as_gdoc(self, src, name=None, parents=()):
+        """Uploads a Microsoft Word (.docx) file to Google drive as Google Document.
+
+        :type src: string
+        :param src: Path to the csv file to be uploaded.
+
+        :type name: string
+        :param name: File name on Google Drive, default to the source file name.
+
+        :type parents: list
+        :param parents: List of Google Drive parent folder IDs.
+
+        :rtype: string
+        :returns: Uploaded file id.
+        """
+
+        return self.upload(
+            src=src,
+            mime_type='application/vnd.google-apps.document',
+            name=name,
+            parents=parents,
+        )
+
     def download(self, file_id, dest):
         """Downloads a file.
 
@@ -119,11 +161,11 @@ class Drive(Service):
         with open(dest, 'wb') as fp:
             incremental_download(fp, request)
 
-    def export_gdoc(self, file_id, dest, mime_type):
+    def export_as(self, file_id, dest, mime_type):
         """Exports a Google docs file.
 
         Can't use apiclient.http.MediaIoBaseDownload when exporting a Google docs
-        file because of infinite loop bug:
+        file because of infinite loop bug. See:
         https://github.com/google/google-api-python-client/issues/15
 
         :type file_id: string
@@ -145,8 +187,8 @@ class Drive(Service):
         with open(dest, 'wb') as fp:
             fp.write(content)
 
-    def export_gsheets_as_csv(self, file_id, dest):
-        """Exports Google Sheets as CSV file.
+    def export_gsheet_as_csv(self, file_id, dest):
+        """Exports a Google Sheet (first sheet only) as CSV file.
 
         :type file_id: string
         :param file_id: Google Sheet file ID.
@@ -156,4 +198,17 @@ class Drive(Service):
         """
 
         mime_type = 'text/csv'
-        self.export_gdoc(file_id, dest=dest, mime_type=mime_type)
+        self.export_as(file_id, dest=dest, mime_type=mime_type)
+
+    def export_gdoc_as_docx(self, file_id, dest):
+        """Exports a Google Doc as Microsoft Word (.docx) file.
+
+        :type file_id: string
+        :param file_id: Google Sheet file ID.
+
+        :type dest: string
+        :param dest: Destination file path.
+        """
+
+        mime_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        self.export_as(file_id, dest=dest, mime_type=mime_type)
